738176199a81a24e0f335808f2cc8b90f6567d2b
werifu <wohu1363195380@outlook.com>
2021-04-14 18:52:25 +0800

    init

---
+++ stories.test.js
@@ -0,0 +1,9 @@
+import { assertEquals } from 'https://Deno.land/std/testing/asserts.ts';
+ 
+// import { mapStory } from './stories.js';
+ 
+console.log("test starts");
+
+Deno.test('maps to a smaller story with formatted date', () => {
+  assertEquals(2, 2);
+});
